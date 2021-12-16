cae5e24c09c74de5f28f3730032f131027c81fd6
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2021-12-16 17:36:07 +1100

    Create report command

---
+++ src/commands/report.ts
@@ -0,0 +1,42 @@
+import { CommandInteraction } from "discord.js";
+import * as Bonjour from "../core";
+
+Bonjour.useCommandRegistry().register({
+  name: "report",
+  description: "Reports DM Spam",
+  ephemeral: true,
+  options: [
+    {
+      name: "user",
+      description: "User to report.",
+      type: "USER",
+      required: true,
+    },
+  ],
+  permissionLevel: 0,
+});
+
+Bonjour.useCommand(
+  "report",
+  async (interaction: CommandInteraction): Bonjour.CommandResponsePromise => {
+    const user = interaction.options.getUser("user", true);
+    try {
+      const logChannel = await interaction.client.channels.fetch(
+        "800938251145314334"
+      );
+      if (logChannel?.isText()) {
+        logChannel.send({
+          embeds: [
+            {
+              title: "Spam Report",
+              description: `${interaction.user} reported ${user} for DM spam.`,
+            },
+          ],
+        });
+      }
+    } catch {
+      //ignored
+    }
+    return `Thanks for reporting ${user}. Your report has been received and will be reviewed by the moderation team.`;
+  }
+);
