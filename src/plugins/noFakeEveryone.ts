b47a3218a4e5483be2e91126728d836c4145ce4c
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2021-12-17 19:24:50 +1100

    Finalise no fake everyone plugin

--- src/plugins/noFakeEveryone.ts
+++ src/plugins/noFakeEveryone.ts
@@ -1,16 +1,27 @@
 import { Message, MessageMentions } from "discord.js";
 import * as Bonjour from "../core";
 
-Bonjour.useEvent("messageCreate", (message: Message) => {
-  let reply = "";
+Bonjour.useEvent("messageCreate", async (message: Message) => {
   const { mentions } = message;
-  if (message.content.match(MessageMentions.EVERYONE_PATTERN)) {
-    reply += "matches regex for everyone/here - ";
+  if (!message.content.match(MessageMentions.EVERYONE_PATTERN)) {
+    return;
   }
   if (mentions.everyone) {
-    reply += "mentions everyone/here";
+    return;
   }
-  if (reply.length > 0) {
-    message.reply(reply);
+  try {
+    await message.delete();
+    const reply = await message.channel.send(
+      `${message.author}, please do not attempt to ping everyone/here!`
+    );
+    setTimeout(async () => {
+      try {
+        await reply.delete();
+      } catch {
+        //ignored
+      }
+    }, 5000);
+  } catch {
+    //ignored
   }
 });
