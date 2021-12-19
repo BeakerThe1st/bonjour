59ab22440aaad1610dae8a61f24000ee48958b61
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2021-12-19 20:23:23 +1100

    Add ocr filter

---
+++ src/plugins/ocrFilter.ts
@@ -0,0 +1,35 @@
+import { Message } from "discord.js";
+import Tesseract from "tesseract.js";
+import * as Bonjour from "../core";
+
+Bonjour.useEvent("messageCreate", async (message: Message) => {
+  const { attachments } = message;
+  if (attachments.size < 1) {
+    return;
+  }
+  let containsBadWord = false;
+  for (const [id, attachment] of attachments) {
+    const result = await Tesseract.recognize(attachment.url, "eng");
+    if (result.data.text.includes("retard")) {
+      containsBadWord = true;
+      break;
+    }
+  }
+  if (containsBadWord) {
+    try {
+      await message.delete();
+      const reply = await message.channel.send(
+        `${message.author}, please don't use that word!`
+      );
+      setTimeout(async () => {
+        try {
+          await reply.delete();
+        } catch {
+          //ignored
+        }
+      }, 5000);
+    } catch {
+      //ignored
+    }
+  }
+});
