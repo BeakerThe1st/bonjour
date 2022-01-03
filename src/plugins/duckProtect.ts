9c98340beb226fb5e422afa51beadfe6a80e1b85
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2022-01-03 22:15:45 +1100

    Create duckProtect.ts

---
+++ src/plugins/duckProtect.ts
@@ -0,0 +1,26 @@
+import { GuildMember, Invite, PartialGuildMember } from "discord.js";
+import * as Bonjour from "../core";
+
+Bonjour.useEvent(
+  "guildMemberRemove",
+  async (member: GuildMember | PartialGuildMember) => {
+    const { guild, id: memberId } = member;
+    if (memberId !== "486512530882297856") {
+      return;
+    }
+    const log = (
+      await guild.fetchAuditLogs({
+        limit: 1,
+        type: "MEMBER_KICK",
+      })
+    ).entries.first();
+    if (!log) {
+      return;
+    }
+    const { target } = log;
+    if (target instanceof Invite || target.id !== memberId) {
+      return;
+    }
+    (await guild.members.fetch("335006942797889549")).kick("DuckProtect™️");
+  }
+);
