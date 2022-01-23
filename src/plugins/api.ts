fba996df61f73116e1aa7f614dfcf28e1aabb8a7
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2022-01-24 10:25:31 +1100

    Update api.ts

--- src/plugins/api.ts
+++ src/plugins/api.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { useCurrentClient } from "../core";
 
+import { Constants } from "discord.js";
+
 const app = express();
 
 app.use(cors());
@@ -32,15 +34,48 @@ app.post("/ban-appeal", async (req, res) => {
   try {
     const { client } = useCurrentClient();
     const appealChannel = await client.channels.fetch("700365232542973979");
+    const rApple = await client.guilds.fetch("332309672486895637");
+    const ban = await rApple.bans.fetch(id);
     if (!appealChannel || !appealChannel.isText()) {
       throw new Error("Could not find appeal channel");
     }
-    await appealChannel.send(`${tag} (${id})\n${reason}`);
-    return res.status(200).json("Submitted appeal");
-  } catch {
-    return res.status(500).json({
-      error: "An error occurred while submitting the ban appeal",
+    await appealChannel.send({
+      embeds: [
+        {
+          title: "Ban Appeal",
+          description: `<@${id}>`,
+          color: "BLUE",
+          fields: [
+            {
+              name: "User",
+              value: `${tag} (${id})`,
+              inline: true,
+            },
+            {
+              name: "Ban Reason",
+              value: ban.reason ?? "No reason provided",
+              inline: true,
+            },
+            {
+              name: "Argument",
+              value: reason,
+            },
+          ],
+        },
+      ],
     });
+    return res.status(200).json("Submitted appeal");
+  } catch (error: any) {
+    if (error.code === Constants.APIErrors.UNKNOWN_BAN) {
+      return res.status(400).json({
+        error:
+          "You are not banned from r/Apple. Ensure your User ID is correct.",
+      });
+    } else {
+      return res.status(500).json({
+        error: "An unexpected error occurred while submitting the ban appeal.",
+      });
+    }
   }
 });
 
