89ff32e5678020847db88880ec830e776d56c109
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2022-01-23 23:57:44 +1100

    Add ban appeal endpoint

--- src/plugins/api.ts
+++ src/plugins/api.ts
@@ -22,6 +22,28 @@ app.get("/guild-info", async (req, res) => {
   });
 });
 
+app.post("/ban-appeal", async (req, res) => {
+  const { tag, id, reason } = req.body;
+  if (!tag || !id || !reason) {
+    return res.status(400).json({
+      error: "Missing required parameters",
+    });
+  }
+  try {
+    const { client } = useCurrentClient();
+    const appealChannel = await client.channels.fetch("700365232542973979");
+    if (!appealChannel || !appealChannel.isText()) {
+      throw new Error("Could not find appeal channel");
+    }
+    await appealChannel.send(`${tag} (${id})\n${reason}`);
+    return res.status(200).json("Submitted appeal");
+  } catch {
+    return res.status(500).json({
+      error: "An error occurred while submitting the ban appeal",
+    });
+  }
+});
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   useCurrentClient().client.emit("debug", `API server started on port ${port}`);
