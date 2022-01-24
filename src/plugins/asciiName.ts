b65d0722c33ff8a54bcea80f742cb7ebe91ff4c3
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2022-01-25 08:16:50 +1100

    Add more logging

--- src/plugins/asciiName.ts
+++ src/plugins/asciiName.ts
@@ -4,29 +4,26 @@ import { useEvent } from "../core";
 
 const foldNickname = async (member: GuildMember) => {
   const { displayName } = member;
+  console.log(`update for ${displayName}`);
   const newName =
     ASCIIFolder.foldReplacing(displayName).substring(0, 32) ||
     `Illegal Nickname ${Math.floor(1000 + Math.random() * 9000)}`;
   console.log(`Folded ${displayName} to ${newName}`);
   if (newName !== displayName) {
-    try {
-      await member.setNickname(newName, "Illegal Nickname");
-      await member.send({
-        embeds: [
-          {
-            author: {
-              name: member.guild.name,
-              iconURL: member.guild.iconURL() ?? undefined,
-            },
-            title: "Nickname Changed",
-            description: `Your display name was found to violate server rules. Your nickname was changed to **${newName}**.`,
-            color: "RED",
+    await member.setNickname(newName, "Illegal Nickname");
+    await member.send({
+      embeds: [
+        {
+          author: {
+            name: member.guild.name,
+            iconURL: member.guild.iconURL() ?? undefined,
           },
-        ],
-      });
-    } catch {
-      //ignored
-    }
+          title: "Nickname Changed",
+          description: `Your display name was found to violate server rules. Your nickname was changed to **${newName}**.`,
+          color: "RED",
+        },
+      ],
+    });
   }
 };
 
