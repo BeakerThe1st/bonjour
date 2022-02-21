157611ee61e41fa915c598fe2b8731394e205d79
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2022-02-22 07:46:50 +1100

    Update antiPhishing.ts

--- src/plugins/antiPhishing.ts
+++ src/plugins/antiPhishing.ts
@@ -4,17 +4,18 @@ import { Message } from "discord.js";
 
 Bonjour.useEvent("messageCreate", async (message: Message) => {
   const { content } = message;
-  if (await stopPhishing.checkMessage(content)) {
-    await message.delete();
-    const reply = await message.channel.send(
-      `${message.author}, please do not send phishing links!`
-    );
-    setTimeout(() => {
-      try {
-        reply.delete();
-      } catch {
-        //ignored
-      }
-    }, 5000);
+  if (!(await stopPhishing.checkMessage(content))) {
+    return;
   }
+  await message.delete();
+  const reply = await message.channel.send(
+    `${message.author}, please do not send phishing links!`
+  );
+  setTimeout(async () => {
+    try {
+      await reply.delete();
+    } catch {
+      //ignored
+    }
+  }, 5000);
 });
