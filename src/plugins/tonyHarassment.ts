0f12227cd62fba8be572bbeb3fba23917a0f144e
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2022-02-27 20:25:31 +1100

    Make tony harassment async

--- src/plugins/tonyHarassment.ts
+++ src/plugins/tonyHarassment.ts
@@ -1,8 +1,8 @@
 import { Message } from "discord.js";
 import * as Bonjour from "../core";
 
-Bonjour.useEvent("messageCreate", (message: Message) => {
+Bonjour.useEvent("messageCreate", async (message: Message) => {
   if (message.author.id === "754983736705286186") {
-    message.react("👀");
+    await message.react("👀");
   }
 });
