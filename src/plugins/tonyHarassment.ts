345246fe8359b590388a62db91a681334b4c413e
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2022-02-27 21:43:21 +1100

    Delete tonyHarassment.ts

--- src/plugins/tonyHarassment.ts
+++
@@ -1,8 +0,0 @@
-import { Message } from "discord.js";
-import * as Bonjour from "../core";
-
-Bonjour.useEvent("messageCreate", async (message: Message) => {
-  if (message.author.id === "754983736705286186") {
-    await message.react("👀");
-  }
-});
