fb7db3d7927c09e320c12da7004676389e2480d3
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2022-03-05 15:58:00 +1100

    Update event.ts

--- src/commands/event.ts
+++ src/commands/event.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, MessageEmbed } from "discord.js";
+import { MessageEmbed } from "discord.js";
 import * as Bonjour from "../core";
 
 Bonjour.useCommandRegistry().register({
@@ -7,35 +7,33 @@ Bonjour.useCommandRegistry().register({
   permissionLevel: 0,
 });
 
-Bonjour.useCommand(
-  "event",
-  (interaction: CommandInteraction): Bonjour.CommandResponse => {
-    const event = {
-      name: "Peek Performance",
-      timestamp: 1646762400000,
-      image: "https://i.imgur.com/PLMut3e.png",
-    };
-    const afterEvent = Date.now() > event.timestamp;
-    const embed = new MessageEmbed({
-      title: `${event.name}`,
-      description: `Apple's ${event.name} event ${
-        afterEvent ? "started" : "starts"
-      } at <t:${event.timestamp}>}}`,
-      image: {
-        url: event.image,
+Bonjour.useCommand("event", (): Bonjour.CommandResponse => {
+  const event = {
+    name: "Peek Performance",
+    timestamp: 1646762400000,
+    image: "https://i.imgur.com/PLMut3e.png",
+  };
+  const afterEvent = Date.now() > event.timestamp;
+  const embed = new MessageEmbed({
+    title: `${event.name}`,
+    description: `Apple's ${event.name} event ${
+      afterEvent ? "started" : "starts"
+    } at <t:${event.timestamp}>`,
+    color: "#1afefd",
+    thumbnail: {
+      url: event.image,
+    },
+    fields: [
+      {
+        name: "Time to Event",
+        value: `Event ${afterEvent ? "began" : "begins"} <t:${
+          event.timestamp / 1000
+        }:R>`,
       },
-      fields: [
-        {
-          name: "Time to Event",
-          value: `Event ${afterEvent ? "began" : "begins"} <t:${
-            event.timestamp / 1000
-          }:R>}`,
-        },
-      ],
-      footer: {
-        text: `All times shown in your local time zone.`,
-      },
-    });
-    return embed;
-  }
-);
+    ],
+    footer: {
+      text: `All times shown in your local time zone.`,
+    },
+  });
+  return embed;
+});
