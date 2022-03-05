878c3003d9734d3dc9269765707f5611b66b6b6a
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2022-03-05 15:35:07 +1100

    Add event info command

---
+++ src/commands/event.ts
@@ -0,0 +1,41 @@
+import { CommandInteraction, MessageEmbed } from "discord.js";
+import * as Bonjour from "../core";
+
+Bonjour.useCommandRegistry().register({
+  name: "event",
+  description: "Displays information about the next Apple event.",
+  permissionLevel: 0,
+});
+
+Bonjour.useCommand(
+  "event",
+  (interaction: CommandInteraction): Bonjour.CommandResponse => {
+    const event = {
+      name: "Peek Performance",
+      timestamp: 1646762400000,
+      image: "https://i.imgur.com/PLMut3e.png",
+    };
+    const afterEvent = Date.now() > event.timestamp;
+    const embed = new MessageEmbed({
+      title: `${event.name}`,
+      description: `Apple's ${event.name} event ${
+        afterEvent ? "started" : "starts"
+      } at <t:${event.timestamp}>}}`,
+      image: {
+        url: event.image,
+      },
+      fields: [
+        {
+          name: "Time to Event",
+          value: `Event ${afterEvent ? "began" : "begins"} <t:${
+            event.timestamp / 1000
+          }:R>}`,
+        },
+      ],
+      footer: {
+        text: `All times shown in your local time zone.`,
+      },
+    });
+    return embed;
+  }
+);
