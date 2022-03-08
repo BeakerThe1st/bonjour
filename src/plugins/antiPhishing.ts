ecc3e56b2ef059c5de86a70a67a8660d71dfe2b3
ElijahPepe <business@elijahpepe.com>
2022-03-08 10:13:16 -0700

    Replace stop-discord-phishing with Phisherman

--- src/plugins/antiPhishing.ts
+++ src/plugins/antiPhishing.ts
@@ -1,10 +1,29 @@
 import * as Bonjour from "../core";
-import stopPhishing from "stop-discord-phishing";
+import axios from 'axios';
 import { Message } from "discord.js";
 
 Bonjour.useEvent("messageCreate", async (message: Message) => {
   const { content } = message;
-  if (!(await stopPhishing.checkMessage(content))) {
+  const domainRegex = /(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/g;
+  let domain: string;
+  let malicious = false;
+  try {
+    if (domainRegex.exec(content) === null) {
+      return;
+    };
+    domain = domainRegex.exec(content)![0];
+    try {
+      await axios.get(`https://api.phisherman.gg/v1/domains/` + domain)
+        .then(res => {
+          malicious = res.data;
+      })
+    } catch (err) {
+      console.log(err);
+    }
+  } catch (error) {
+    // ignored
+  };
+  if (!malicious) {
     return;
   }
   await message.delete();
