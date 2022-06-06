d4d56109025181331daf3d81f728a80cea3eee4c
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2022-06-06 21:13:18 +1000

    Change eb role

--- src/commands/eb.ts
+++ src/commands/eb.ts
@@ -26,7 +26,7 @@ Bonjour.useCommand(
       throw new Error("That command must only be run in a guild.");
     }
     const eventBlocklistRole = await interaction.guild.roles.fetch(
-      "950717415816319026"
+      "983327276475236402"
     );
     if (!eventBlocklistRole) {
       throw new Error("Cannot find event blocklist role");
